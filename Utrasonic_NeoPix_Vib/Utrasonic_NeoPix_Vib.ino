50fbe7ce0dd667e4eb86c69ca7273ff8ee365d49
Justin Luke <justin.luke@berkeley.edu>
2017-11-26 22:37:34 -0800

    added 2nd rear neopixel strip in neopix_vib.ino; created master.ino

--- Utrasonic_NeoPix_Vib/Utrasonic_NeoPix_Vib.ino
+++ Utrasonic_NeoPix_Vib/Utrasonic_NeoPix_Vib.ino
@@ -1,6 +1,7 @@
 #include <Adafruit_NeoPixel.h>
 #define PIN 6
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(16, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(12, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(13, PIN, NEO_GRB + NEO_KHZ800);
 
 // defines pins numbers
 const int trigPin = 9;
@@ -13,7 +14,6 @@ int toAvg[5];
 int total;
 const int motorPin = 3;
 boolean alert = 0;
-//const int ledPin = 6;
 int trigDist = 90; 
 
 // Global RGB values, change to suit your needs
@@ -22,20 +22,22 @@ int g = 0;
 int b = 255;
 
 void setup() {
-  // put your setup code here, to run once:
 pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
 pinMode(echoPin, INPUT); // Sets the echoPin as an Input
 pinMode(motorPin, OUTPUT);
-pinMode(ledPin, OUTPUT); //Sets LED as Output
 
 Serial.begin(9600); // Starts the serial communication
 //sets up neopixel lights
 strip.begin();
   strip.setBrightness(20);
   strip.show();
-  pinMode(switchPin, INPUT);
 
+strip2.begin();
+  strip2.setBrightness(20);
+  strip2.show();
 }
+
+
 
 void loop() {
  total = 0;
@@ -101,14 +103,19 @@ return alert;
 void allOff() {
   strip.clear();
   strip.show();
+  strip2.clear();
+  strip2.show();
 }
 
 // Turns the NeoPixels on, according to RGB settings
 void activate() {
-  for( int i = 0; i < 16; i++ ) 
+  for( int i = 0; i < 16; i++ ) {
        strip.setPixelColor(i, r,g,b );
+       strip2.setPixelColor(i, r,g,b );
+  }
       
   strip.show();
+  strip2.show();
 }
 
 void blinky(int repeats) {
